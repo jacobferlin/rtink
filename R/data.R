f66c13ae3f240f7e38ccd7bc0c28ee508f7b7e62
Jacob Ferlin <jacob.ferlin@me.com>
2019-04-20 13:09:54 +0200

    Separeate auth functions and data functions.

---
+++ R/data.R
@@ -0,0 +1,44 @@
+get <- function(token, path) {
+
+  # Build url
+  url <- paste0(url_base(), path)
+
+  # Build header
+  token_type   <- stringr::str_to_title(token$credentials$token_type)
+  access_token <- token$credentials$access_token
+  auth_str     <- paste(token_type, access_token)
+
+  # GET
+  r <- httr::GET(
+    url = url,
+    httr::add_headers(Authorization = auth_str)
+  )
+
+  # Assert Success
+  assertthat::assert_that(
+    httr::status_code(r) == 200,
+    msg = paste("No success, status code", httr::status_code(r))
+  )
+
+  # Parse JSON
+  parsed <- jsonlite::fromJSON(httr::content(r, "text"), simplifyVector = FALSE)
+
+  structure(
+    list(
+      content = parsed,
+      path = path,
+      response = r
+    ),
+    class = "tink_api"
+  )
+}
+
+get_accounts <- function(token) {
+  get(token, "/accounts/list")
+}
+
+print.tink_api <- function(x, ...) {
+  cat("<Tink ", x$path, ">\n", sep = "")
+  str(x$content)
+  invisible(x)
+}
