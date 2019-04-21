2525b796e1bdadbffd83c7c9e78fea39c6b20a88
Jacob Ferlin <jacob.ferlin@me.com>
2019-04-21 16:39:19 +0200

    Convert UNIX Epoch Time.

--- R/pretty.R
+++ R/pretty.R
@@ -1,27 +1,39 @@
 pretty <- function(l, cols) {
 
+  # All cols?
   if (length(cols) == 1 && cols == "all") cols <- names(l[[1]])
 
+  # Helper function to extract all named col from list
+  # and put into a tibble with a single column
   create_tibble_col <- function(col_name) {
     tibble::tibble(!!col_name := purrr::map(l, col_name))
   }
 
+  # Extract all columns, bind them into one tibble,
+  # and flatten into correct type/class
   df_list <- lapply(cols, function(x) create_tibble_col(x))
   df      <- dplyr::bind_cols(df_list)
-  df      <- apply(df, 2, function(x) flatten_dynamic(x))
+  df      <- purrr::map(df, flatten_dynamic)
   df      <- tibble::as_tibble(df)
+
+  # Convert UNIX Epoch Time in milliseconds to datetime
+  is_unix_epoch_ms <- function(col) is.numeric(col) && all(col > 1e+12)
+  as_datetime_ms   <- function(ms) lubridate::as_datetime(ms * 1e-3)
+  df <- dplyr::mutate_if(df,
+                         is_unix_epoch_ms,
+                         dplyr::funs(as_datetime_ms))
 
   df
 }
 
 pretty_accounts <- function(parsed_content,
-                            cols = c("id", "name", "balance")) {
+                            cols = c("id", "accountNumber", "name", "balance")) {
   l <- parsed_content[["accounts"]]
   pretty(l, cols = cols)
 }
 
 pretty_transactions <- function(parsed_content,
-                                cols = c("id", "description", "amount")) {
+                                cols = c("id", "description", "amount", "date")) {
   l <- parsed_content
   pretty(l, cols = cols)
 }
